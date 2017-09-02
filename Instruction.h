9d27af595ba7b669da721b13280c8097032bb741
Matt Jenkins <matt@majenko.co.uk>
2017-09-03 00:35:59 +0100

    Initial import

---
+++ Instruction.h
@@ -0,0 +1,29 @@
+#ifndef _INSTRUCTION_H
+#define _INSTRUCTION_H
+
+#include <DisplayCore.h>
+
+namespace Fonts {
+    extern const uint8_t Instruction10[];
+    extern const uint8_t Instruction11[];
+    extern const uint8_t Instruction12[];
+    extern const uint8_t Instruction13[];
+    extern const uint8_t Instruction14[];
+    extern const uint8_t Instruction15[];
+    extern const uint8_t Instruction16[];
+    extern const uint8_t Instruction17[];
+    extern const uint8_t Instruction18[];
+    extern const uint8_t Instruction19[];
+    extern const uint8_t Instruction20[];
+    extern const uint8_t Instruction21[];
+    extern const uint8_t Instruction22[];
+    extern const uint8_t Instruction23[];
+    extern const uint8_t Instruction24[];
+    extern const uint8_t Instruction25[];
+    extern const uint8_t Instruction26[];
+    extern const uint8_t Instruction27[];
+    extern const uint8_t Instruction28[];
+    extern const uint8_t Instruction29[];
+    extern const uint8_t Instruction30[];
+};
+#endif
